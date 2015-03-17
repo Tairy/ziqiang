a34478d57c7785f07872d82d350b9ada996bdcc9
Tairy <tairyguo@gmail.com>
2015-03-17 15:13:35 +0000

    setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,16 @@ class Setting
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
 
-  def get(key)
-    self.find(key)
+  def self.get_option(option)
+    self.first[option]
+  end
+
+  def self.set_option(key, value)
+    if self.first.nil?
+      object = self.new(key => value)
+      object.save
+    else
+      self.first.update_attribute(key,value)
+    end
   end
 end
